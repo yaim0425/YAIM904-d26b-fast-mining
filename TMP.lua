fe1a1feaff56d3450307018602de42a71adebeba
yaim0425 <yaim0425@gmail.com>
2025-11-07 16:38:53 -0500

    Primer commit

---
+++ TMP.lua
@@ -0,0 +1,18 @@
+--[[
+    git checkout main
+    git checkout code -- .
+    git add .
+    git commit -m "Prioridad a code"
+    git push origin main
+]]
+
+--[[ Idea de MOD
+    --- Permitir construcción en el espacio
+    Entity.collision_mask.layers.space_tile = nil
+
+    --- Mercado intercambiando items y fluid por diner
+
+    --- Juego de frio y caliente
+
+    --- Pocesar los cadaveres de los aliens
+]]
